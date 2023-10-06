12bb9f2f75c691fb8066bdccbc94c6e064f4520d
ahmedkhan935 <ahmedkhanraza935@gmail.com>
2023-10-06 23:30:06 +0500

    complete

--- script.js
+++ script.js
@@ -1,239 +1,14 @@
-// var jobData = [];
-// async function getJobs() {
-// await fetch("data.json").then(response => {
-//     return response.json();
-// }
-// ).then(data => {
-//     jobData = data;
-// });
-// }
-// const jobList = $("#job-list");
-
-// // Function to generate job listings from JSON data
-// async function generateJobListings() {
-//     if(jobData.length === 0)
-//         await getJobs();
-//     $("#job-list").html("");    
-//     jobData.forEach(job => {
-//         const listItem = $("<li>").addClass("job-item");
-
-
-//         // Create the HTML structure for each job listing
-//         listItem.html(`
-//             <img src="${job.logo}" alt="${job.company} Logo">
-//             <div class="job-details">
-//                 <div class="company-info">
-//                     <h2>${job.company}</h2>
-//                     ${job.new ? '<div class="new">New!</div>' : ''}
-//                     ${job.featured ? '<div class="featured">Featured</div>' : ''}
-//                 </div>
-//                 <h3>${job.position}</h3>
-//                 <ul class="job-meta">
-//                     <li>${job.postedAt}</li>
-//                     <li>${job.contract}</li>
-//                     <li>${job.location}</li>
-//                 </ul>
-//             </div>
-//             <div class="job-tags">
-//                 <span>${job.role}</span>
-//                 <span>${job.level}</span>
-//                 ${job.languages.map(language => `<span>${language}</span>`).join('')}
-//                 ${job.tools.map(tool => `<span>${tool}</span>`).join('')}
-//             </div>
-//             <button class="close-icon" onclick="deleteJob(${jobData.indexOf(job)})">&times;</button>
-//         `);
-
-//         jobList.append(listItem);
-//     });
-// }
-// function deleteJob(index) {
-//     jobData.splice(index, 1);
-//     $("#job-list").html("");
-//     generateJobListings();
-// }
-// // Call the function to generate job listings
-// // Select elements using jQuery
-// const addJob = $("#add-job-button");
-// const close = $("#close-add-job-button");
-// const add = $("#add");
-
-// // Add click event handlers using jQuery
-// addJob.on("click", () => {
-//     $("#add-job-modal").css("display", "flex");
-// });
-
-// close.on("click", () => {
-//     $("#add-job-modal").css("display", "none");
-// });
-
-// add.on("click", () => {
-    
-
-//     const logo = $("#imageLink").val();
-// const title = $("#jobTitle").val();
-// const isNew = $("#isNew").prop("checked");
-// const isFeatured = $("#isFeatured").prop("checked");
-// const company = $("#companyName").val();
-// const days = $("#days").val();
-// const location = $("#location").val();
-// const jobType = $("#jobType").val();
-// const languages = $("#languages").val().split(",");
-// const tools = $("#tools").val().split(",");
-
-// if(logo=="" || title=="" || company=="" || days=="" || location=="" || jobType=="" || languages==""){
-//     alert("Please fill all the fields");
-//     return;
-// }
-
-//     //prit all
-//     const listing={
-//         logo:logo,
-//         company:company,
-//         new:isNew,
-//         featured:isFeatured,
-//         position:title,
-//         postedAt:days,
-//         contract:jobType,
-//         location:location,
-//         languages:languages,
-//         tools:tools
-//     }
-//     jobData.push(listing);
-//     document.getElementById("add-job-modal").style.display="none";
-//     generateJobListings();
-
-// });
-
-// const filterInput = $("#filterInput");
-// const selectedTagsContainer =$("#selectedTags");
-
-// // Listen for clicks on job tags
-// jobList.on("click", function(event) {
-//     if (event.target.tagName === "SPAN") {
-//         const clickedTag = event.target.textContent;
-//         addTagToSearchBar(clickedTag);
-//     }
-// });
-
-// // Function to add a tag to the search bar
-// function addTagToSearchBar(tag) {
-//     var flag=false;
-//     const childrens=$(selectedTagsContainer).children().toArray();
-//         childrens.forEach(child=>{
-//             if(child.id==tag){
-//                 alert("Tag already added");
-//                 flag=true;
-//                 return;
-//             }
-
-//         });
-//     if(flag)
-//         return;    
-//     // Create a tag element with a cross sign
-//     const tagElement = $('<div>')
-//     .addClass('selected-tag')
-//     .attr('id', tag)
-//     .html(`
-//     <span>${tag}</span>
-//     <button onclick="removeTag(${tag})" class="remove-tag">×</button>
-//     `);
-//     console.log(tag);
-  
-
-//     // Append the tag to the selected tags container
-    
-//     selectedTagsContainer.append(tagElement);
-//     const children=$(selectedTagsContainer).children().toArray();
-   
-//         var filterTags=[];
-//         children.forEach(child=>{
-//             if(filterTags.includes(child.id)==false){
-//                 filterTags.push(child.id);
-//             }
-
-//         });
-//         generateFilteredJobListings(filterTags);
-
-//     // Update the filter input value with the selected tags
-    
-// }
-
-// // Function to remove a tag from the search bar
-// function removeTag(tag) {
-//     tag=tag.id;
-//     const tagToRemove = $( `#${tag}`);
-//     if (tagToRemove) {
-//         console.log(tagToRemove);
-//         tagToRemove.remove();
-//         const childrens=$(selectedTagsContainer).children().toArray();
-//         var filterTags=[];
-//         childrens.forEach(child=>{
-//             filterTags.push(child.id);
-//         });
-//         if(filterTags.length==0)
-//             generateJobListings();
-//         else
-//             generateFilteredJobListings(filterTags);
-       
-       
-//     }
-// }
-// async function generateFilteredJobListings(filterTags) {
-//     if(jobData.length === 0)
-//         await getJobs();
-//     $("#job-list").html("");
-//     jobData.forEach(job => {
-//         var flag=false
-//         filterTags.forEach(tag=>{
-//             if(job.role==tag || job.level==tag || job.languages.includes(tag) || job.tools.includes(tag))
-//                 flag=true;
-//         });
-//         if(flag){
-//                 const listItem = $("<li>").addClass("job-item");
-        
-        
-//                 // Create the HTML structure for each job listing
-//                 listItem.html(`
-//                     <img src="${job.logo}" alt="${job.company} Logo">
-//                     <div class="job-details">
-//                         <div class="company-info">
-//                             <h2>${job.company}</h2>
-//                             ${job.new ? '<span class="new">New!</span>' : ''}
-//                             ${job.featured ? '<span class="featured">Featured</span>' : ''}
-//                         </div>
-//                         <h3>${job.position}</h3>
-//                         <ul class="job-meta">
-//                             <li>${job.postedAt}</li>
-//                             <li>${job.contract}</li>
-//                             <li>${job.location}</li>
-//                         </ul>
-//                     </div>
-//                     <div class="job-tags">
-//                         <span>${job.role}</span>
-//                         <span>${job.level}</span>
-//                         ${job.languages.map(language => `<span>${language}</span>`).join('')}
-//                         ${job.tools.map(tool => `<span>${tool}</span>`).join('')}
-//                     </div>
-//                     <button class="close-icon" onclick="deleteJob(${jobData.indexOf(job)})">&times;</button>
-//                 `);
-        
-//                 jobList.append(listItem);
-         
-        
-//     }});
-// }
-
-// generateJobListings();
-
-
-
 
 var jobData = [];
 
 async function getJobs() {
-    await fetch("data.json").then(response => {
-        return response.json();
-    }).then(data => {
+    // await fetch("data.json").then(response => {
+    //     return response.json();
+    // }).then(data => {
+    //     jobData = data;
+    // });
+    await $.get("data.json", function (data) {
+        
         jobData = data;
     });
 }
@@ -351,7 +126,7 @@ jobList.on("click", "span", function () {
     addTagToSearchBar(clickedTag);
 });
 
-// Function to add a tag to the search bar
+
 function addTagToSearchBar(tag) {
     var flag = false;
     const childrens = $(selectedTagsContainer).children().toArray();
@@ -365,7 +140,14 @@ function addTagToSearchBar(tag) {
     if (flag)
         return;
     
-    // Create a tag element with a cross sign
+    const clearButton = $("#clear-button");
+    console.log(clearButton.length);
+    const html=`<div class="clear-button"><button id="clear-button" onclick="clearTags()">Clear</button></div>`;
+    if(clearButton.length==0){
+        const filter=$("#filter-bar");
+        filter.append(html);
+    }
+    
     const tagElement = $('<div>')
         .addClass('selected-tag')
         .attr('id', tag)
@@ -386,7 +168,13 @@ function addTagToSearchBar(tag) {
     }).get();
     generateFilteredJobListings(filterTags);
 }
-
+function clearTags()
+{
+    selectedTagsContainer.html("");
+    $("#clear-button").remove();
+    filterInput.val("");
+    generateJobListings();
+}
 // Function to remove a tag from the search bar
 function removeTag(tag) {
     $(`#${tag}`).remove();
@@ -481,6 +269,5 @@ jobList.on("click", "h3", function () {
     const job = jobData[jobIndex];
     showJobDetails(job);
 });
-
 
 generateJobListings();
